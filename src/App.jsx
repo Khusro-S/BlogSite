5de7076a39679b9259539101f7386152a1fe258e
Khusro-S <khusro.sakhi20@gmail.com>
2024-02-16 20:10:07 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hellow World</h1>
+        <h2>Hellow World</h2>
+        <h3>Hellow World</h3>
+        <h4>Hellow World</h4>
+      </div>
+    </>
+  );
+}
+
+export default App;
